1ccf7cc0cc19d07be9016a0eef33f3e37cb656e5
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2024-05-10 12:26:15 -0500

    Numberlike

---
+++ Sources/BigArray/Numberlike.swift
@@ -0,0 +1,10 @@
+//
+//  Numberlike.swift
+//  
+//
+//  Created by Dr. Brandon Wiley on 5/10/24.
+//
+
+import Foundation
+
+public typealias Numberlike = Numeric & Equatable & Comparable & Hashable & Codable & HasOne & HasZero
